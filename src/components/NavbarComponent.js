20188baa93c050566842151bd069ed353c41eed3
John Dunstan <johntdunstan@gmail.com>
2021-02-11 17:02:36 -0700

    Further config

--- src/components/NavbarComponent.js
+++ src/components/NavbarComponent.js
@@ -2,46 +2,61 @@ import React from 'react'
 import CountdownContainer from './countdown/CountdownContainerComponent'
 import Stats from './stats/StatsComponent'
 import About from './about/AboutComponent'
-import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom'
-
-// TO DO: Change my links to <NavLink>'s in order to add active classes automatically
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  NavLink,
+} from 'react-router-dom'
 
 // NOTE: When I start having multiple lists, I'll want to start using 'match' with React Router
+
+// TODO: Set up a basic 404-page to route to
 
 function Navbar(props) {
   return (
     <Router>
-      <div>
-        <nav>
-          <ul>
-            <li>
-              <Link to="/">Home</Link>
-            </li>
-            <li>
-              <Link to="/stats">Stats</Link>
-            </li>
-            <li>
-              <Link to="/about">About</Link>
-            </li>
-          </ul>
-        </nav>
+      <nav>
+        <h1>Moment Counter</h1>
+        <ul>
+          <li>
+            <NavLink to="/">Home</NavLink>
+          </li>
+          <li>
+            <NavLink activeClassName="active-nav-item" to="/stats">
+              Stats
+            </NavLink>
+          </li>
+          <li>
+            <NavLink activeClassName="active-nav-item" to="/about">
+              About
+            </NavLink>
+          </li>
+        </ul>
+      </nav>
 
-        <Switch>
-          <Route path="/about">
-            <About />
-          </Route>
-          <Route path="/stats">
-            <Stats />
-          </Route>
-          <Router path="/">
-            <CountdownContainer
-              showCompleted={props.showCompleted}
-              name={props.name}
-              items={props.items}
-            />
-          </Router>
-        </Switch>
-      </div>
+      <Switch>
+        <Route exact path="/stats">
+          <Stats />
+        </Route>
+        <Route exact path="/about">
+          <About />
+        </Route>
+        <Route exact path="/">
+          <CountdownContainer
+            showCompleted={props.showCompleted}
+            name={props.name}
+            items={props.items}
+          />
+        </Route>
+        <Route path="*">
+          <CountdownContainer
+            showCompleted={props.showCompleted}
+            name={props.name}
+            items={props.items}
+          />
+        </Route>
+      </Switch>
     </Router>
   )
 }
--- src/components/countdown/DisplayItemsComponent.js
+++ src/components/countdown/DisplayItemsComponent.js
@@ -3,9 +3,11 @@ import React from 'react'
 function DisplayItems(props) {
   function RenderItem(item) {
     if (!item.isComplete) {
-      return <button>Function Item {item.id} | NOT Complete</button>
+      return (
+        <button key={item.id}>Function Item {item.id} | NOT Complete</button>
+      )
     } else if (props.showCompleted && item.isComplete) {
-      return <button>Function Item {item.id} | Complete</button>
+      return <button key={item.id}>Function Item {item.id} | Complete</button>
     }
   }
 
