da3b8697a2ef152e344c43e90c51761b5d4e8d11
John Dunstan <johntdunstan@gmail.com>
2021-02-21 08:49:00 -0700

    Add styling to 404

--- src/components/404Component.js
+++ src/components/404Component.js
@@ -1,12 +1,16 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faHome } from '@fortawesome/free-solid-svg-icons'
 
 function PageNotFound() {
   return (
     <>
-      <h1>Page not found 🤦🏼‍♂️</h1>
-      <h2>
-        <Link to="/">Return home</Link>
+      <h1>Page Not Found 🤦🏼‍♂️</h1>
+      <h2 className="content-body percent-span">
+        <Link to="/">
+          Return Home <FontAwesomeIcon icon={faHome} />
+        </Link>
       </h2>
     </>
   )
